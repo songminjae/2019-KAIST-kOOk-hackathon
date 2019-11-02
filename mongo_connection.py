82fb81b6412216375344828e25375695db00865e
songminjae <52192602+songminjae@users.noreply.github.com>
2019-11-02 22:05:49 +0900

    Add files via upload

--- mongo_connection.py
+++ mongo_connection.py
@@ -2,7 +2,7 @@ import pymongo
  
 client = pymongo.MongoClient("localhost", 27017)
 db = client.contactlist
-collection = db.contactlist
+collection = db.essay
  
 def save(name, category):
     collection.save({"name": name, "category": category})
--- server.js
+++ server.js
@@ -3,12 +3,77 @@ var app=express();
 var mongojs=require('mongojs');
 var bodyParser=require('body-parser');
 var db=mongojs('contactlist',['contactlist']);
+var db2=mongojs('contactlist',['name'])
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 
 app.get('/contactlist',function(req,res){
 	console.log('i recieved a get req');
      db.contactlist.find(function(err,docs){
+          console.log(docs);
+          res.json(docs);
+     });
+
+});
+
+app.get('/contactlist/user',function(req,res){
+     console.log('i recieved a get req');
+     db2.songminjae.find(function(err,docs){
+          console.log(docs);
+          res.json(docs);
+     });
+
+});
+
+app.get('/contactlist/relig',function(req,res){
+     console.log('i recieved a get req');
+     db2.religious.find(function(err,docs){
+          console.log(docs);
+          res.json(docs);
+     });
+
+});
+
+app.get('/contactlist/toe',function(req,res){
+     console.log('i recieved a get req');
+     db2.toeic.find(function(err,docs){
+          console.log(docs);
+          res.json(docs);
+     });
+
+});
+
+app.get('/contactlist/poe',function(req,res){
+     console.log('i recieved a get req');
+     db2.poet.find(function(err,docs){
+          console.log(docs);
+          res.json(docs);
+     });
+
+});
+
+
+app.get('/contactlist/ar',function(req,res){
+     console.log('i recieved a get req');
+     db2.art.find(function(err,docs){
+          console.log(docs);
+          res.json(docs);
+     });
+
+});
+
+app.get('/contactlist/ki',function(req,res){
+     console.log('i recieved a get req');
+     db2.kids.find(function(err,docs){
+          console.log(docs);
+          res.json(docs);
+     });
+
+});
+
+app.get('/contactlist/ess',function(req,res){
+     console.log('i recieved a get req');
+     db2.essay.find(function(err,docs){
           console.log(docs);
           res.json(docs);
      });
@@ -22,7 +87,25 @@ app.post('/contactlist',function(req,res){
      });
 });
 
-/*
+
+app.get('/contactlist/:id',function(req,res){
+     var id=req.params.id;
+     console.log(id);
+     db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,docs){
+          res.json(docs);
+     });
+});
+
+app.get('/contactlist/:id',function(req,res){
+     var id=req.params.id;
+     console.log(id);
+     db2.songminjae.findOne({_id:mongojs.ObjectId(id)},function(err,docs){
+          res.json(docs);
+     });
+});
+
+
+
 app.delete('/contactlist/:id',function(req,res){
      var id=req.params.id;
      console.log(id);
@@ -47,7 +130,7 @@ app.put('/contactlist/:id',function(req,res){
           });
 
 }); 
-*/
+
 
 app.listen(3000);
 console.log("server up");
